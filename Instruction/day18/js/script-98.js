6ea235cfd6bd38f5162c0d870bf54b1c80821d17
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-05-19 23:35:17 +0300

    v55

---
+++ Instruction/day18/js/script-98.js
@@ -0,0 +1,7 @@
+import showAlert, { showConfirm } from "./message.js";
+document.getElementById("btnAlert").addEventListener("click", ()=>{
+    showAlert("Hello Module")
+});
+document.getElementById("btnConfirm").addEventListener("click", ()=>{
+    showConfirm("Are you sure tu update?");
+});
